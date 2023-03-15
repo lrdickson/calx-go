1c4a531d615bc47ab65f76d7943cf072db3c2c89
Lyndsey Dickson <lyndseyrd@gmail.com>
2023-03-14 23:18:14 -0400

    Finally get list binding to work

--- internal/views/mainview.go
+++ internal/views/mainview.go
@@ -1,6 +1,8 @@
 package views
 
 import (
+	"log"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/data/binding"
@@ -9,24 +11,57 @@ import (
 	"github.com/lrdickson/ssgo/internal/viewmodels"
 )
 
+type variableInfo struct {
+	code   string
+	name   string
+	output string
+}
+
+func checkErrFatal(message string, err error) {
+	if err != nil {
+		log.Fatal(message, err)
+	}
+}
+
 func NewMainView() *fyne.Container {
-	mainViewModel := viewmodels.NewMainViewModel()
 
 	// Display the output
-	outputListBind := binding.NewStringList()
-	outputListDisplay := widget.NewListWithData(outputListBind,
+	variables := binding.NewUntypedList()
+	variableList := widget.NewListWithData(
+		variables,
 		func() fyne.CanvasObject {
-			return newVariableView2()
+			name := widget.NewLabel("Template")
+			output := widget.NewLabel("Output")
+			return container.NewBorder(name, nil, nil, nil, output)
 		},
-		func(i binding.DataItem, o fyne.CanvasObject) {
-			o.(*VariableView).Bind(i.(binding.String))
+		func(item binding.DataItem, obj fyne.CanvasObject) {
+			// Get the variable
+			v, err := item.(binding.Untyped).Get()
+			checkErrFatal("Failed to get variable data:", err)
+			variable := v.(variableInfo)
+
+			// Set the output
+			output := obj.(*fyne.Container).Objects[0].(*widget.Label)
+			output.SetText(variable.output)
+			output.Refresh()
+
+			// Set the name
+			name := obj.(*fyne.Container).Objects[1].(*widget.Label)
+			name.SetText(variable.name)
+			name.Refresh()
 		})
-	newVariableButton := widget.NewButton("New", func() { outputListBind.Append("Hello") })
+	newVariableButton := widget.NewButton("New", func() {
+		// Create a new variable
+		newVariable := variableInfo{"", "NewVariable", ""}
+		variables.Append(newVariable)
+		variableList.Refresh()
+	})
 
 	// Display data from the variable
 	variableDisplay1 := widget.NewLabel("")
 
 	// Create the editor
+	mainViewModel := viewmodels.NewMainViewModel()
 	editorCodeBind := binding.BindString(&mainViewModel.EditorCode)
 	variableEditor := widget.NewMultiLineEntry()
 	variableEditor.Bind(editorCodeBind)
@@ -40,7 +75,7 @@ func NewMainView() *fyne.Container {
 	// Put everything together
 	content := container.NewBorder(
 		nil, nil,
-		container.NewBorder(variableDisplay1, newVariableButton, nil, nil, outputListDisplay),
+		container.NewBorder(variableDisplay1, newVariableButton, nil, nil, variableList),
 		nil,
 		container.NewBorder(nil, runButton, nil, nil, variableEditor))
 
